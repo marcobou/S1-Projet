055497069af16d822854d38397cca916050bdc81
maboucher-northstella <80048016+marcobou@users.noreply.github.com>
2021-09-30 17:07:05 -0400

    added comments + fix tab

--- ProjetRobot-S1/src/main.cpp
+++ ProjetRobot-S1/src/main.cpp
@@ -2,58 +2,59 @@
 #include <LibRobus.h>
 
 /**
- * Circumference of the wheels
+ * Circumference of the wheels (Formula = 2*PI*r)
  * 
- * Furmula of circumferece = 2*PI*r
  * PI rounded to 3
  * r = 1.5 inches rounded to 4cm
  */
 const int CIRCUMFERENCE = 2 * 3 * 4;
+/** Speed for the left motor when going forward */
 const float LEFT_FORWARD_SPEED = 0.45f;
+/** Speed for the right motor when going forward */
 const float RIGHT_FORWARD_SPEED = 0.471f;
-const int SLOW_DISTANCE = 30;
 
 void forward(int distance);
 
 void setup()
 {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("I am born");
-
-  BoardInit();
+    Serial.begin(9600);
+    BoardInit();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  forward(218);
+    forward(218);
 
-  while(true) {
+    // Infinite loop to stop the execution of the program
+    while(true) {
     
-  }
+    }
 }
 
 /**
- * Fonction pour avancer le robot en ligne droite
+ * Function that makes the robot go forward on a specified distance.
  * 
- * @param[in] distance Distance que le robot doit parcourir en cm.
+ * @param[in] distance The distance that the robot moves in cm.
  */
 void forward(int distance)
 {
-  float nbWheelTurnsFast = 1.0f * distance / CIRCUMFERENCE;
+    // Number of turns the wheels must do to cover the distance.
+    float nbWheelTurnsFast = 1.0f * distance / CIRCUMFERENCE;
+    // Number of pulses that the motors will have to do.
+    int nbPulsesFast = nbWheelTurnsFast * 3200;
 
-  int nbPulsesFast = nbWheelTurnsFast * 3200;
+    MOTOR_SetSpeed(0, LEFT_FORWARD_SPEED);
+    MOTOR_SetSpeed(1, RIGHT_FORWARD_SPEED);
 
-  MOTOR_SetSpeed(0, LEFT_FORWARD_SPEED);
-  MOTOR_SetSpeed(1, RIGHT_FORWARD_SPEED);
+    // Will make the robot go forward until the motors exceed the number needed pulses
+    while (ENCODER_Read(0) < nbPulsesFast)
+    {
+        // TODO read the encoder for the right motor too.
+        delay(100);
+    }
 
-  while (ENCODER_Read(0) < nbPulsesFast)
-  {
-    delay(100);
-  }
+    ENCODER_ReadReset(0);
 
-  ENCODER_ReadReset(0);
-  MOTOR_SetSpeed(0, 0.0f);
-  MOTOR_SetSpeed(1, 0.0f);
+    MOTOR_SetSpeed(0, 0.0f);
+    MOTOR_SetSpeed(1, 0.0f);
 }
