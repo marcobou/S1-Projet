b9b4a9a4415db64ba3bc2c74b0da8df90d577f8a
maboucher-northstella <80048016+marcobou@users.noreply.github.com>
2021-10-12 14:42:26 -0400

    accélérer + ralentissement

--- ProjetRobot-S1/src/main.cpp
+++ ProjetRobot-S1/src/main.cpp
@@ -1,169 +1,209 @@
 #include <Arduino.h>
 #include <LibRobus.h>
+#include <math.h>
 
-/**
- * Circumference of the wheels (Formula = 2*PI*r)
- * 
- * PI rounded to 3
- * r = 1.5 inches rounded to 4cm
- */
-const int WHEEL_CIRCUMFERENCE = 2 * 3 * 4;
-/** The circumference of the circle that the robot draws when  */
-const float TURNING_ROBOT_CIRCUMFERENCE = 2 * 3.14f * 19;
-/** Speed for the left motor when going forward */
-const float LEFT_FORWARD_SPEED = 0.40f;
-/** Speed for the right motor when going forward */
-const float RIGHT_FORWARD_SPEED = 0.40f;
-/** Ponderation factor for the PID */
-const float KP = 0.0001f;
-/** Number of pulses a motor does by cycle */
-const int PULSES_BY_TURN = 3200;
+#define ENCODE_TOUR_ROUE 3200     // Nombre de ticks d'encodeur pour faire un tour
+#define PULSE_PAR_ROND 7979*2
+#define TAILLE_ROUE_PO 9.42477796 // Taille des roues en pouces
+#define TAILLE_ROUE_ROBOTA 7.63
+#define TAILLE_ROUE_ROBOTB 7.55
+ 
+#define BASE_SPEED 0.2 
+#define BASE_TURN_SPEED 0.3                 // Vitesse de base
+#define MINUS_BASE_SPEED -0.4           // Vitesse de base negative
+#define QUARTER_BASE_SPEED BASE_SPEED/4 // 1/4 de la vitesse de base pour départs et arrets plus doux
+#define FACTEUR_CORRECTION 0.0004       // Facteur de correction pour le PID
+#define CORRECTION_PAR_TOUR 8           // Nombre de fois que le PID ajuste les valeurs de vitesses par tour
+ 
+const float TAILLE_ROUE_CM = TAILLE_ROUE_ROBOTA * 3.141592;
 
-void forward(long distance);
-void turn(int angle, int turning_side);
+void avance_tour_roue(float nbTour); 
+void avance (float x);
+void turn (float angle);
 
-void setup()
+/* 
+Fonction de setup pour initialiser le robot
+*/
+void setup() { 
+  // put your setup code here, to run once: 
+  Serial.begin(9600); 
+  BoardInit(); 
+  MOTOR_SetSpeed(LEFT,0); 
+  MOTOR_SetSpeed(RIGHT,0); 
+  Serial.println("Start"); 
+} 
+
+void loop() 
 {
-    //Serial.begin(9600);
-    BoardInit();
+  avance (220);
+  turn(-90.0);
+  avance (20);
+  turn(90);
+  avance(20);
+  turn(90);
+  avance(20);
+  turn(-90);
+  avance(10);
+  turn(45);
+  avance(34);
+  turn(-90);
+  avance(55);
+  turn(45);
+  avance(27);
+  turn(20);
+  avance(100);
+  turn(-20);
 
-    MOTOR_SetSpeed(LEFT, 0.0f);
-    MOTOR_SetSpeed(RIGHT, 0.0f);
+  turn(180);
 
-    forward(210);
-    turn(90, LEFT);
-    forward(40);
-    turn(90, RIGHT);
-    forward(35);
-    turn(90, RIGHT);
-    forward(20);
-    turn(45, LEFT);
+  turn(20);
+  avance(100);
+  turn(-20);
+  avance(27);
+  turn(-45);
+  avance(55);
+  turn(90);
+  avance(34);
+  turn(-45);
+  avance(10);
+  turn(90);
+  avance(20);
+  turn(-90);
+  avance(20);
+  turn(-90);
+  avance (20);
+  turn(90.0);
+  avance (220);
+
+  MOTOR_SetSpeed(LEFT, 0);
+  MOTOR_SetSpeed(RIGHT, 0);
+
+  while(1);
+
 }
 
-void loop()
-{
-
+void avance (float x){
+  x = (1.0*x/TAILLE_ROUE_CM);
+  avance_tour_roue(x);
+  
 }
-
-void end_action()
-{
-    // Reset the encoders for the next action.
-    ENCODER_Reset(LEFT);
-    ENCODER_Reset(RIGHT);
-
-    MOTOR_SetSpeed(LEFT, 0.0f);
-    MOTOR_SetSpeed(RIGHT, 0.0f);
-
-    delay(200);
-}
-
-
-void forward(long distance)
-{
-    // Having a distance of 0 or less means that the robot does not move since he can't move back.
-    if (distance <= 0)
+void turn (float angle){
+  
+  long nbPulseRight = 0;
+  ENCODER_Reset(RIGHT); // Variables contenant les dernières valeures d'encodeurs
+  long nbPulseLeft = 0;
+  ENCODER_Reset(LEFT); 
+  float convert = abs(angle/360*PULSE_PAR_ROND);
+  Serial.println(convert);
+  Serial.println(angle);
+  //tourne a gauche
+  if (angle<0)
+  {
+    MOTOR_SetSpeed(RIGHT, BASE_TURN_SPEED);
+    //MOTOR_SetSpeed(LEFT, MINUS_BASE_SPEED);
+    while(convert>nbPulseRight)
     {
-        return;
-    }
+      _delay_us(10);
+      nbPulseRight = ENCODER_Read(RIGHT);
 
-    // Reset the encoders just in case there were not already reset.
-    ENCODER_Reset(LEFT);
-    ENCODER_Reset(RIGHT);
+      }
+    MOTOR_SetSpeed(RIGHT, 0);
+   // MOTOR_SetSpeed(LEFT, 0);
 
-    // Number of turns the wheels must do to cover the distance.
-    float nb_wheel_turns = 1.0f * distance / WHEEL_CIRCUMFERENCE;
+  }
+   //tourne a droite
+   else if (angle>0 && angle!=180){
+     MOTOR_SetSpeed(LEFT, BASE_TURN_SPEED);
+     while(convert>nbPulseLeft)
+     {
+      _delay_us(10);
+      nbPulseLeft = ENCODER_Read(LEFT);
+        }
+      MOTOR_SetSpeed(LEFT, 0);
+   }
+   else if (angle == 180){
+     MOTOR_SetSpeed(LEFT, BASE_TURN_SPEED);
+     MOTOR_SetSpeed(RIGHT, -BASE_TURN_SPEED);
+     while((convert/2)>nbPulseLeft)
+     {
+      _delay_us(10);
+      nbPulseLeft = ENCODER_Read(LEFT);
+        }
+      MOTOR_SetSpeed(LEFT, 0);
+      MOTOR_SetSpeed(RIGHT, 0);
 
-    // Number of pulses that the motors will have to do.
-    long nb_pulses_distance = nb_wheel_turns * PULSES_BY_TURN;
+   }
 
-    // A counter that keeps track of the total number of pulses made with a motor
-    // since we will reset the encoder after each reading
-    long total_pulses_left = 0;
-    long total_pulses_right = 0;
 
-    float left_speed = 0.25f;
-    float right_speed = 0.25f;
+_delay_us(10);
+//delay(1000);
+ }
 
-    float left_speed_modifier = 0.0f;
 
-    MOTOR_SetSpeed(LEFT, left_speed);
-    MOTOR_SetSpeed(RIGHT, right_speed);
 
-    bool go_slow = true;
 
-    // Will make the robot go forward until the motors exceed the needed number of pulses
-    while (total_pulses_left < nb_pulses_distance && total_pulses_right < nb_pulses_distance)
+
+/*
+Cette fonction prend en entrée un nombre entier et fait tourner les
+roues un nombre de tour égal a ce nombre entier
+*/
+void avance_tour_roue(float nbTour) 
+{ 
+  long nbPulseRight = 0;
+  long nbPulseLeft = 0;
+
+  ENCODER_Reset(RIGHT); // Variables contenant les dernières valeures d'encodeurs
+  ENCODER_Reset(LEFT); 
+ 
+  float speedRatio = 0; // Ratio utilisé pour ajuster le rapport de vitesse entre les deux roues
+
+    long pulse_to_do = nbTour * 3200;
+    long pulses_left = 0;
+    long pulses_right;
+
+    MOTOR_SetSpeed(LEFT, (BASE_SPEED)); 
+    MOTOR_SetSpeed(RIGHT, BASE_SPEED);
+
+    float added_speed = 0.0001f;
+    float reduce_speed = added_speed * 1.1f;
+    float current_speed = BASE_SPEED;
+    float max_speed = 1.0f;
+    float min_speed = 0.2f;
+
+    while(pulses_left < pulse_to_do)
     {
-        delay(100);
+        pulses_left = ENCODER_Read(LEFT);
+        pulses_right = ENCODER_Read(RIGHT);
 
-        if (go_slow && 1.0f * total_pulses_left / PULSES_BY_TURN >= 1.0f)
+        if (pulses_left * 1.0f / pulse_to_do < 0.5f)
         {
-            go_slow = false;
-            left_speed = LEFT_FORWARD_SPEED - left_speed_modifier;
-            right_speed = RIGHT_FORWARD_SPEED;
-            MOTOR_SetSpeed(LEFT, left_speed);
-            MOTOR_SetSpeed(RIGHT, right_speed);
+            if (current_speed < max_speed)
+            {
+                current_speed += added_speed;
+            }
+        }
+        else
+        {
+            if (current_speed > min_speed)
+            {
+                current_speed -= reduce_speed;
+            }
         }
 
-        // Reset the encoders after the reading because we want the number of pulses between each readings,
-        // not the number of pulses since the beginning.
-        long pulses_right = ENCODER_ReadReset(RIGHT);
-        long pulses_left = ENCODER_ReadReset(LEFT);
+        MOTOR_SetSpeed(LEFT, current_speed + speedRatio); 
+        MOTOR_SetSpeed(RIGHT, current_speed);
 
-        total_pulses_left += pulses_left;
-        total_pulses_right += pulses_right;
+        speedRatio = (pulses_right-pulses_left)*FACTEUR_CORRECTION;
 
-        // ===== PID =====
-
-        // If there is a difference between the pulses of the right motor and the left one.
-        if (pulses_left != pulses_right)
-        {
-            int speed_error = pulses_left - pulses_right;
-            float added_left_speed = (speed_error * KP);
-
-            left_speed_modifier += added_left_speed;
-
-            left_speed = left_speed - added_left_speed;
-            MOTOR_SetSpeed(LEFT, left_speed);
-        }
-
-        // ===============
+        _delay_us(100);
     }
 
-    end_action();
-}
-
-/**
- * Function that makes the robot turn of a specified angle.
- * 
- * @param[in] angle Angle in degrees that the robot must turn. Turn clockwise if bigger than 0.
- */
-void turn(int angle, int turning_side)
-{
-    // Simple validation, angle must be between 1 and 180 degrees and the int for the turning 
-    // side must be 0 (left) or 1 (right).
-    if (angle < 1 || angle > 180 || (turning_side != LEFT && turning_side != RIGHT))
-    {
-        return;
-    }
-
-    // The distance that the wheel must cover to turn to the specified degree.
-    // Formula for the measurement of a circular arc : (center_angle * circle_circumference) / 360
-    float circular_arc_distance = (angle * TURNING_ROBOT_CIRCUMFERENCE) / 360;
-    // Number of turns the wheels must do to cover the distance.
-    float nb_wheel_turn = circular_arc_distance / WHEEL_CIRCUMFERENCE;
-    // Number of pulses that the motors will have to do.
-    long nb_pulses = nb_wheel_turn * PULSES_BY_TURN;
-
-    // If the robot turns left, it means that it's the right wheel that must works.
-    int turning_motor = (turning_side == LEFT) ? RIGHT : LEFT;
-
-    MOTOR_SetSpeed(turning_motor, 0.3f);
-
-    while(ENCODER_Read(turning_motor) < nb_pulses)
-    {
-        delay(20);
-    }
-
-    end_action();
+  
+ 
+  // Stop les moteurs
+  MOTOR_SetSpeed(0,0); 
+  MOTOR_SetSpeed(1,0); 
+  nbPulseLeft = ENCODER_ReadReset(LEFT);
+  nbPulseRight = ENCODER_ReadReset(RIGHT);
+  _delay_us(10);
 }
